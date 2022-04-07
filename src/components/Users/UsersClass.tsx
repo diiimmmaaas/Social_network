828ba62b1825d6f94e65591ce514312f0dd8adad
Dmitry Sokolov <dm.sokolov1994@gmail.com>
2022-04-07 13:02:07 +0300

    refactor component UserClass

--- src/components/Users/UsersClass.tsx
+++ src/components/Users/UsersClass.tsx
@@ -12,17 +12,16 @@ interface UsersPropsType {
 }
 
 class UsersClass extends React.Component<UsersPropsType> {
-    getUsers = () => {
-        if (this.props.users.length === 0) {
-            axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
-                this.props.setUsers(response.data.items)
-            })
-        }
+    constructor(props: UsersPropsType) {
+        super(props);
+        axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
+            this.props.setUsers(response.data.items)
+        })
     }
+
     render() {
         return (
             <div>
-                <button onClick={this.getUsers}>Get Users</button>
                 {this.props.users.map(u => <div key={u.id}>
                 <span>
                     <div>
