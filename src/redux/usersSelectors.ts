c80e05b5bc79cd26f3bfbda15124b695ca4dc05c
diiimmmaaas <dm.sokolov1994@gmail.com>
2022-05-11 14:49:44 +0300

    reselect

--- src/redux/usersSelectors.ts
+++ src/redux/usersSelectors.ts
@@ -1,8 +1,14 @@
 import {RootStoreType} from "./reduxStore";
+import {createSelector} from "reselect";
 
 export const getUsers = (state: RootStoreType) => {
     return state.usersPage.users
 }
+
+// reselect, не нужный кусок кода, так как селектор очень простой и логика его высосана из пальца
+export const getUserSuperSelector = createSelector( getUsers ,(users) => {
+    return users.filter(u => true)
+} )
 
 export const getPageSize = (state: RootStoreType) => {
     return state.usersPage.pageSize
